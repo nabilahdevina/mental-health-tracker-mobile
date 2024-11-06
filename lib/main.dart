f5cb0f51f605d53d8147d9c5d176bab4a407d875
“nabilahdevina” <dhevina10@gmail.com>
2024-11-06 14:06:45 +0700

    tutor 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.deepPurple,
- ).copyWith(secondary: Colors.deepPurple[400]),
+        ).copyWith(secondary: Colors.deepPurple[400]),
         useMaterial3: true,
       ),
       home: MyHomePage(),
